4ef95ff5750cd18c52332b47c675d8d5b59865a5
Carlos Gomes <carlos.algms@gmail.com>
2021-01-19 00:04:56 +0100

    Removed try block to break the CI on error

--- scripts/upload-extension/upload.js
+++ scripts/upload-extension/upload.js
@@ -16,27 +16,23 @@ const owner = 'carlos-algms';
 const repo = 'ps-wishlist';
 
 void (async () => {
-  try {
-    const release = await octokit.repos.getReleaseByTag({
-      owner,
-      repo,
-      tag: process.env.TAG,
-    });
+  const release = await octokit.repos.getReleaseByTag({
+    owner,
+    repo,
+    tag: process.env.TAG,
+  });
 
-    const result = await octokit.repos.uploadReleaseAsset({
-      owner,
-      repo,
-      release_id: release.data.id,
-      data: fs.createReadStream(file),
-      headers: {
-        'Content-Type': mime.lookup(file) || 'application/octet-stream',
-        'Content-Length': fs.statSync(file).size,
-      },
-      name: path.basename(file),
-    });
+  const result = await octokit.repos.uploadReleaseAsset({
+    owner,
+    repo,
+    release_id: release.data.id,
+    data: fs.createReadStream(file),
+    headers: {
+      'Content-Type': mime.lookup(file) || 'application/octet-stream',
+      'Content-Length': fs.statSync(file).size,
+    },
+    name: path.basename(file),
+  });
 
-    console.log('Extension uploaded:', result.data.browser_download_url);
-  } catch (error) {
-    console.error(error);
-  }
+  console.log('Extension uploaded:', result.data.browser_download_url);
 })();
